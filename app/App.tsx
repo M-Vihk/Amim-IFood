580d7153c30d0aa58988177e028baa79a4ddf8c2
Deyvison Samuel Gomes do Nascimento <deyvisonnascimento2025@gmail.com>
2025-11-02 02:10:58 -0300

    commit tela de cadastro

--- app/App.tsx
+++ app/App.tsx
@@ -1,11 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, ScrollView } from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* Header */}
+      <View style={styles.header}>
+        <Text style={styles.logo}>Amim</Text>
+      </View>
+
+      {/* Conteúdo principal */}
+      <ScrollView contentContainerStyle={styles.content}>
+        <Text style={styles.title}>Cadastro</Text>
+
+        {/* Campos de entrada */}
+        <TextInput style={styles.input} placeholder="Nome" placeholderTextColor="#fff" />
+        <TextInput style={styles.input} placeholder="E-mail" placeholderTextColor="#fff" keyboardType="email-address" />
+        <TextInput style={styles.input} placeholder="Senha" placeholderTextColor="#fff" secureTextEntry />
+        <TextInput style={styles.input} placeholder="Confirmar senha" placeholderTextColor="#fff" secureTextEntry />
+        <TextInput style={styles.input} placeholder="dd/mm/aaaa" placeholderTextColor="#fff" />
+        <TextInput style={styles.input} placeholder="Selecione o gênero" placeholderTextColor="#fff" />
+
+        {/* Botão */}
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>CADASTRAR</Text>
+        </TouchableOpacity>
+      </ScrollView>
     </View>
   );
 }
@@ -13,8 +34,52 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#fee8f2', 
+  },
+  header: {
+    backgroundColor: '#ff95a5ff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingVertical: 20,
+  },
+  logo: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#fff',
+  },
+  content: {
+    alignItems: 'center',
+    padding: 20,
+  },
+  title: {
+    fontSize: 40,
+    fontWeight: 'bold',
+    color: '#ff4da6',
+    marginBottom: 20,
+  },
+  input: {
+    width: '100%',
+    backgroundColor: '#fdd0e5',
+    borderRadius: 25,
+    padding: 12,
+    marginVertical: 6,
+    color: '#646464ff',
+    fontWeight: 'bold',
+  },
+  button: {
+    backgroundColor: '#ff4da6',
+    paddingVertical: 14,
+    borderRadius: 25,
+    width: '90%',
+    alignItems: 'center',
+    marginTop: 15,
+    
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 16,
   },
 });
+
+export default App;
